de01bd7e8992cdc7a2cbffee17e4c31d1ce9d9b2
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-02-24 21:30:48 +0100

    Classe ParseData

---
+++ app/src/main/java/com/example/adrien/projetmobilel3/util/ParseData.java
@@ -0,0 +1,20 @@
+package com.example.adrien.projetmobilel3.util;
+
+import android.content.Intent;
+
+/**
+ * Created by MrkJudge on 24/02/2017.
+ */
+
+public class ParseData {
+
+    public static byte[] parseIntent(Intent intent) {
+        //TODO non commencé
+        return null;
+    }
+
+    public static Intent parseByteArray(byte[] bytes) {
+        //TODO non commencé
+        return null;
+    }
+}

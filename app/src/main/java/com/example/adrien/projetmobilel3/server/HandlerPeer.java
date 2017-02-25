ede115095b24d9349b95fc47bd292002415ca0e4
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-02-25 14:58:23 +0100

    ajout d'un 'handler' de handler qui récolte tous les points reçus

--- app/src/main/java/com/example/adrien/projetmobilel3/server/HandlerPeer.java
+++ app/src/main/java/com/example/adrien/projetmobilel3/server/HandlerPeer.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -17,7 +18,7 @@ import java.util.ArrayList;
  * Created by MrkJudge on 24/02/2017.
  */
 
-//TODO non terminé
+//TODO non terminé + discuter de l'AsyncTask ou Thread
 public class HandlerPeer extends AsyncTask<Socket,ArrayList<Point>,String>{
 
     private ServerP2P server;
@@ -25,12 +26,20 @@ public class HandlerPeer extends AsyncTask<Socket,ArrayList<Point>,String>{
     private OutputStream os;
 
     private boolean stop = false;
-
     private int pointPacketLenght = Point.getByteLength();
+    private final ArrayList<Point> points = new ArrayList<>();
 
     public HandlerPeer(ServerP2P server) {
         this.server = server;
         server.getHandlers().add(this);
+    }
+
+    public OutputStream getOutPutSteam() { return os; }
+    public ArrayList<Point> getPoints() { return points; }
+    public synchronized ArrayList<Point> gatherPoints() {
+        ArrayList<Point> pointsGathered = new ArrayList<>(getPoints());
+        getPoints().clear();
+        return pointsGathered;
     }
 
     @Override
@@ -47,7 +56,7 @@ public class HandlerPeer extends AsyncTask<Socket,ArrayList<Point>,String>{
                 //TODO méthode de déserialization + envoie des données vers l'application
 
             }
-            s.close();
+            s.close();points.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -62,7 +71,6 @@ public class HandlerPeer extends AsyncTask<Socket,ArrayList<Point>,String>{
         //TODO non commencé
     }
 
-    public OutputStream getOutPutSteam() {
-        return os;
-    }
+
+
 }

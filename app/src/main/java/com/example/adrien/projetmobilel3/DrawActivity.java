21f66a17d0b9e52a6b636ab4357cf2d72ad17b1d
ADRIEN BARDES <adrien.bardes@etu.u-pec.fr>
2017-03-14 14:49:32 +0100

    L'interface s'affiche correctement

--- app/src/main/java/com/example/adrien/projetmobilel3/DrawActivity.java
+++ app/src/main/java/com/example/adrien/projetmobilel3/DrawActivity.java
@@ -1,8 +1,11 @@
 package com.example.adrien.projetmobilel3;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +16,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class DrawActivity extends AppCompatActivity {
+import com.example.adrien.projetmobilel3.common.PointPacket;
+import com.example.adrien.projetmobilel3.draw.Draw;
+import com.example.adrien.projetmobilel3.draw.Point;
+
+public class DrawActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);*/
+        /*setSupportActionBar(toolbar);*/
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -34,9 +41,19 @@ public class DrawActivity extends AppCompatActivity {
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, null, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        Draw draw = (Draw) findViewById(R.id.draw_content);
+        draw.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                ((Draw) v).addEvent(event);
+                return true;
+            }
+        });
 
         /*
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);

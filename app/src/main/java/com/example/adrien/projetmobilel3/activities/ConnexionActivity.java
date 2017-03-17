8a1964596902cf84fd52a26d6de0a1b589d435d9
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-03-17 21:05:28 +0100

    modification de l'interface + ajout d'un voyant si la wifiP2P est activée

--- app/src/main/java/com/example/adrien/projetmobilel3/activities/ConnexionActivity.java
+++ app/src/main/java/com/example/adrien/projetmobilel3/activities/ConnexionActivity.java
@@ -2,11 +2,14 @@ package com.example.adrien.projetmobilel3.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.example.adrien.projetmobilel3.R;
@@ -23,6 +26,19 @@ public class ConnexionActivity extends Activity {
         setContentView(R.layout.activity_connexion);
 
         Intent parameters = getIntent();
+
+        RadioButton radioButton = ((RadioButton) findViewById(R.id.radioButton));
+        radioButton.setChecked(parameters.getBooleanExtra("isWifiP2pEnabled",false));
+
+        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    buttonView.setTextColor(Color.GREEN);
+                else
+                    buttonView.setTextColor(Color.RED);
+            }
+        });
 
         String[] peersName = parameters.getStringArrayExtra("peersName");
 

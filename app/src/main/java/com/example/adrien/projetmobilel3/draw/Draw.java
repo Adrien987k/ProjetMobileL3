637f31e9c92c19f44a4565a5a011e828d47cba7e
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-03-18 13:53:10 +0100

    javadoc

--- app/src/main/java/com/example/adrien/projetmobilel3/draw/Draw.java
+++ app/src/main/java/com/example/adrien/projetmobilel3/draw/Draw.java
@@ -19,30 +19,97 @@ import com.example.adrien.projetmobilel3.common.PointPacket;
 import java.util.ArrayList;
 
 /**
- * Created by MrkJudge on 24/01/2017.
+ * The Draw class is a view where users will draw.
+ * A listener must be added to catch user's action
+ * and call Draw methods to actually draw.
  */
-
 public class Draw extends View {
 
+    /**
+     * The current color.
+     * When a point is drawn by the local user, his color will be this one.
+     */
     public int color;
+
+    /**
+     * The default color at starting.
+     */
     public static final int DEFAULT_COLOR = Color.BLACK;
 
+    /**
+     * The current stroke.
+     * When a point is drawn by the local user, his stroke will be this one.
+     */
     public int stroke;
+
+    /**
+     * The default stroke at starting.
+     */
     public static final int DEFAULT_STROKE = 10;
 
+    /**
+     * The alpha transparency.
+     * When a point is drawn by the local user, his transparency will be this one.
+     * Note that this feature is not fully yet implemented and alpha cannot be changed.
+     */
     public int alpha;
+
+    /**
+     * The alpha transparency at starting.
+     */
     public static final int DEFAULT_ALPHA = 255;
 
+    /**
+     * The paint of the local user, mainly containing the color and the stroke.
+     * This paint is used when you need to re-draw.
+     */
     private Paint paint;
+
+    /**
+     * The path of the local user.
+     * When a point is draw, he will be added to the path then the whole path
+     * will be drawn.
+     */
     private Path path;
+
+    /**
+     * The canvas where everything is drawn.
+     * Each path from every users is drawn on this canvas,
+     * then the canvas is drawn on the real canvas in onDraw method;
+     * Usually the onDraw method is immediately called when a path is modified.
+     */
     private Canvas myCanvas = new Canvas();
+
+    /**
+     * The bitmap of the canvas.
+     */
     private Bitmap bitMap;
+
+    /**
+     * A path containing every points drawn by the local user.
+     * It's used to save points when the activity need to restart.
+     * Note that this path will be frequently add on a path list and reset.
+     */
     private MyPath myPath;
 
+    /**
+     * An access to the draw activity.
+     */
     private DrawActivity drawActivity;
 
+    /**
+     * An array list containing every points drawn.
+     * It's used to save points when the activity need to restart.
+     * //TODO unused ?
+     */
     private ArrayList<PointPacket> points = new ArrayList<>();
 
+    /**
+     * Create a draw view with the specified color, stroke and transparency.
+     * @param color The color at starting.
+     * @param stroke The stroke at starting.
+     * @param alpha The alpha transparency at startgin.
+     */
     private Draw(Context context, AttributeSet attrs, int color, int stroke, int alpha) {
         super(context,attrs);
         this.color = color;
@@ -53,6 +120,9 @@ public class Draw extends View {
         init();
     }
 
+    /**
+     * Create a draw view with default values of color, stroke and transparency.
+     */
     public Draw(Context context, AttributeSet attrs) {
         this(context,attrs,DEFAULT_COLOR,DEFAULT_STROKE,DEFAULT_ALPHA);
     }
@@ -61,7 +131,11 @@ public class Draw extends View {
         return myCanvas;
     }
 
-    public void init() {
+    /**
+     * Initialize the paint with current color, stroke and transparency.
+     * Also, it's setting some stroke parameters to draw simple stroke.
+     */
+    private void init() {
         paint.setAntiAlias(true);
         paint.setColor(color);
         paint.setStrokeWidth(stroke);
@@ -71,6 +145,14 @@ public class Draw extends View {
         paint.setStrokeCap(Paint.Cap.ROUND);
     }
 
+    /**
+     * Draw a point from the received event.
+     * It extracts coordinates and draw the point with current drawing parameters.
+     * @deprecated Note that it works only for local draw. If you have multiple users,
+     * you should not use this method, because you can't identify the drawer.
+     * @param event The MotionEvent received.
+     */
+    @Deprecated
     public synchronized void addEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
@@ -94,6 +176,11 @@ public class Draw extends View {
         postInvalidate();
     }
 
+    /**
+     * Draw a point and add it to the path list.
+     * @param pointPacket The point to draw.
+     * @param paths The path list to add the point.
+     */
     public synchronized void addPointPacket(PointPacket pointPacket, ArrayList<MyPath> paths) {
 
         if(paths == null)System.out.println(pointPacket);
@@ -132,6 +219,13 @@ public class Draw extends View {
         postInvalidate();
     }
 
+    /**
+     * Draw a point with the specified draw tools.
+     * This method is used when you have multiple users
+     * and each of them have a personal draw tools.
+     * @param drawTools The draw tools to use.
+     * @param pointPacket The point to draw.
+     */
     public synchronized void drawPointPacket(DrawTools drawTools, PointPacket pointPacket) {
         float x = pointPacket.getPoint().getX();
         float y = pointPacket.getPoint().getY();
@@ -154,7 +248,14 @@ public class Draw extends View {
         }
     }
 
-    public synchronized void myLineTo(float x, float y, DrawTools drawTools) {
+    /**
+     * A personal implementation of the method Path.lineTo.
+     * This method must be used instead of Path.lineTo if you have multiple users.
+     * It will prevent you to have superposition conflicts,
+     * because only the new point, and therefore the line to the last point, is drawn.
+     * @param drawTools The draw tools to use.
+     */
+    private synchronized void myLineTo(float x, float y, DrawTools drawTools) {
         Path path = drawTools.getPath();
         Paint paint = drawTools.getPaint();
 
@@ -165,6 +266,15 @@ public class Draw extends View {
         path.moveTo(x,y);
     }
 
+    /**
+     * A personal implementation of the method Path.lineTo.
+     * This method must be used instead of Path.lineTo if you have multiple users.
+     * It will prevent you to have superposition conflicts,
+     * because only the new point, and therefore the line to the last point, is drawn.
+     * @deprecated Note that it only works for local draw. If you have multiple users,
+     * you should not use this method, because you can't identify the drawer.
+     */
+    @Deprecated
     private synchronized void myLineTo(float x, float y) {
         path.lineTo(x, y);
         myCanvas.drawPath(path,paint);
@@ -173,6 +283,12 @@ public class Draw extends View {
         path.moveTo(x,y);
     }
 
+    /**
+     * Draw all paths from the list.
+     * This is called when the activity has restarted and saved points
+     * need to be re-drawn.
+     * @param paths
+     */
     public void drawPaths(ArrayList<MyPath> paths) {
         for(MyPath path: paths) {
             for(PointPacket pointPacket: path.getPointPackets()) {
@@ -201,10 +317,19 @@ public class Draw extends View {
 
     }
 
+    /**
+     * Set the draw activity link.
+     * This method must be called at the start of the activity
+     * if you need to save points in the future.
+     * @param drawActivity
+     */
     public void setDrawActivity(DrawActivity drawActivity) {
         this.drawActivity = drawActivity;
     }
 
+    /**
+     * Clear all known points and clear the drawable area.
+     */
     public synchronized void clear() {
 
         drawActivity.getPaths().clear();
@@ -214,6 +339,12 @@ public class Draw extends View {
     public ArrayList<PointPacket> getPoints() {
         return points;
     }
+
+    /**
+     * Draw all points from the list.
+     * Note that this method can handle multiple users.
+     * @param points The point list to draw.
+     */
     public void setPoints(ArrayList<PointPacket> points) {
         this.points = points;
         for(PointPacket pointPacket: points)
